61b421d0e92a45d625f81243c059ae7c1637438d
조승휘 <ab8868123@gmail,com>
2026-03-22 10:50:32 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -69,3 +69,6 @@ data = {
     'is_spam': [False, False, True, False, False, True, False, False],
     'has_image': [True, False, True, True, False, False, True, True]
 }
+df = pd.DataFrame(data)
+print(df.head())
+
