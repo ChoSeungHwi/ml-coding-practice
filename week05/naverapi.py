51de3da9fb86cb1e0983758dc91fe8936221dc1e
조승휘 <ab8868123@gmail,com>
2026-04-17 19:01:12 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -3,8 +3,8 @@ import urllib.request
 import datetime
 import json
 
-client_id =  'Client ID'
-client_secret = 'Client Secret'
+client_id =  'bj6aHRnJgsD10bL_E38K'
+client_secret = '1khTglJB6Q'
 
 def main():
      
