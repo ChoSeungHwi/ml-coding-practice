1842c57d54f2ee2239d71f5e36339ce45dbaa860
조승휘 <ab8868123@gmail,com>
2026-04-17 13:00:18 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -69,4 +69,6 @@ def getRequestUrl(url):  #[CODE 1]
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
-            print
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+        except Exception as e:
