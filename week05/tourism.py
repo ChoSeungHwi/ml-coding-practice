78c5f09324cd07727906ec046cabcceebd2f33e4
조승휘 <ab8868123@gmail,com>
2026-04-17 12:52:47 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -58,3 +58,8 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
     parameters = "?_type=json&serviceKey=" + ServiceKey         # 인증키
+    if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
+    
