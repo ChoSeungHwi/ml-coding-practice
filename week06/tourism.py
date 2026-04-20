7a3b8d6e917e52c46d2b8c8c78a28f921f00cfdf
조승휘 <ab8868123@gmail,com>
2026-04-20 21:29:08 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -19,4 +19,7 @@ def main():
     ed_cd = "E"
 
     jsonResult, result, natName, dataEND = getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear)  #[CODE 3]
+
+    #파일저장 : csv 파일  
+    columns = ["입국자국가", "국가코드", "입국연월", "입국자 수"]
     
