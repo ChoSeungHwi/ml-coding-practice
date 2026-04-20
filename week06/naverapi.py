a6fa0258e6e68bc816662d825c76621fe9041f54
조승휘 <ab8868123@gmail,com>
2026-04-20 21:54:09 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -15,4 +15,9 @@ def main():
     jsonResult = []
 
     jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
-    
+    total = jsonResponse['total']
+
+    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
+        for post in jsonResponse['items']:
+            cnt += 1
+            
