33e8dfd8101513378b2e3ff1574e068c8d698d87
조승휘 <ab8868123@gmail,com>
2026-04-20 21:53:09 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -7,4 +7,8 @@ client_id =  'bj6aHRnJgsD10bL_E38K'
 client_secret = '1khTglJB6Q'
 
 def main():
+
+    node = 'news'                                             # 크롤링할 대상
+    srcText = input('검색어를 입력하세요: ')
+
     
