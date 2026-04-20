f58d7ab1fec2148db8ec8ff353f79a073dfadc2c
조승휘 <ab8868123@gmail,com>
2026-04-20 21:25:38 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
-
 import urllib.request
 import datetime
+import json
+import pandas as pd
+
+ServiceKey = "dfab2bbf478888cbfe0efd04460a2f0202093ea9fb2874b43fe7e5ee15b5054c"
