224b1f7719d12dbfcdc9461906ee2dc8d0c904fd
조승휘 <ab8868123@gmail,com>
2026-05-27 22:05:24 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -1,2 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
+
+iris = load_iris(as_frame=True)
+X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y_iris = iris.target
